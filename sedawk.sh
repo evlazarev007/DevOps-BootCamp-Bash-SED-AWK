302f74a4a7709ca406d7587547753fa4dd92b3cb
Yevgeniy Lazarev <ev_lazarev@mail.ru>
2022-12-14 22:06:52 +0000

    555

--- sedawk.sh
+++ sedawk.sh
@@ -3,16 +3,20 @@
 cp passwd passwd_new
 
 # Change shell for user saned
-awk -i inplace -F: 'BEGIN {OFS=":"} $1 == "saned" {$7 = "/bin/bash"} {print}' passwd_new
+awk -F: 'BEGIN {OFS=":"} $1 == "saned" {$7 = "/bin/bash"} {print}' passwd_new > passwd_tmp
+mv passwd_tmp passwd_new
 
 # Change shell for user avahi
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
 
 # Save only 1, 3, 5 ,7 columns
-awk -i inplace 'BEGIN{FS=":";OFS=":"}{print $1,$3,$5,$7}' passwd_new
+awk 'BEGIN{FS=":";OFS=":"}{print $1,$3,$5,$7}' passwd_new > passwd_tmp
+mv passwd_tmp passwd_new
+
 
 # Remove all lines containing word daemon
 sed -i '/daemon/d' passwd_new
 
 # Change shell for all users with even UID
-awk -i inplace -F: 'BEGIN {OFS=":"} $2 %2 == 0 {$4 = "/bin/zsh"} { print }' passwd_new
+awk -F: 'BEGIN {OFS=":"} $2 %2 == 0 {$4 = "/bin/zsh"} { print }' passwd_new > passwd_tmp
+mv passwd_tmp passwd_new
