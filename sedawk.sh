bd281f4aa6815801fc1c0032e869bcfc6bc05386
Yevgeniy Lazarev <ev_lazarev@mail.ru>
2022-12-14 22:01:46 +0000

    555

--- sedawk.sh
+++ sedawk.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 cp passwd passwd_new
-awk -i inplace -F: '{if ($1=="saned"){sub("/usr/sbin/nologin","/bin/bash")}\
-print $0}' passwd_new
+
+# Change shell for user saned
+awk -i inplace -F: 'BEGIN {OFS=":"} $1 == "saned" {$7 = "/bin/bash"} {print}' passwd_new
+
+# Change shell for user avahi
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
+
+# Save only 1, 3, 5 ,7 columns
 awk -i inplace 'BEGIN{FS=":";OFS=":"}{print $1,$3,$5,$7}' passwd_new
+
+# Remove all lines containing word daemon
 sed -i '/daemon/d' passwd_new
-awk -i inplace -F: '{if ($2 %2 == 0) {sub($4,"/bin/zsh")} print $0}' passwd_new
+
+# Change shell for all users with even UID
+awk -i inplace -F: 'BEGIN {OFS=":"} $2 %2 == 0 {$4 = "/bin/zsh"} { print }' passwd_new
