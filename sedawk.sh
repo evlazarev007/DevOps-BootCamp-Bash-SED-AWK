1ede6386806d557b9743108dc317b176e168e59b
Yevgeniy Lazarev <ev_lazarev@mail.ru>
2022-12-14 21:33:40 +0000

    333

--- sedawk.sh
+++ sedawk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cp passwd passwd_new
-awk -i inplace -F: '{if ($1=="saned") {sub("/usr/sbin/nologin","/bin/bash")} print $0}' passwd_new
+awk -i inplace -F: '{if ($1=="saned"){sub("/usr/sbin/nologin","/bin/bash")}\
+print $0}' passwd_new
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' passwd_new
 awk -i inplace 'BEGIN{FS=":";OFS=":"}{print $1,$3,$5,$7}' passwd_new
 sed -i '/daemon/d' passwd_new
