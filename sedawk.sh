41e4cb6b1a2efa5e0ecc48fc3e9090e2d5393af8
Yevgeniy Lazarev <ev_lazarev@mail.ru>
2022-12-14 19:29:01 +0000

    11

--- sedawk.sh
+++ sedawk.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
-# Put you code here
+awk -F: '{if ($1=="saned") {sub("/usr/sbin/nologin","/bin/bash")} print $0}' ./passwd > ./passwd_new
+sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' ./passwd_new
+cp ./passwd_new ./passwd_new2
+cut -d : -f1,3,5,7- < ./passwd_new2 > ./passwd_new
+rm ./passwd_new2
+sed -i '/daemon/d' ./passwd_new
+awk -i inplace -F: '{if ($2 %2 == 0) {sub($4,"/bin/zsh")} print $0}' ./passwd_new
