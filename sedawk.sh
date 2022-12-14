621563a1982f68db6b0805a0264928fed2feb113
Yevgeniy Lazarev <ev_lazarev@mail.ru>
2022-12-14 20:27:35 +0000

    222

--- sedawk.sh
+++ sedawk.sh
@@ -5,5 +5,4 @@ awk -i inplace -F: '{if ($1=="saned") {sub("/usr/sbin/nologin","/bin/bash")} pri
 sed -i '/avahi:/s/\/usr\/sbin\/nologin/\/bin\/bash/' ./passwd_new
 awk -i inplace 'BEGIN{FS=":";OFS=":"}{print $1,$3,$5,$7}' ./passwd_new
 sed -i '/daemon/d' ./passwd_new
-sed -i '/Daemon/d' ./passwd_new
 awk -i inplace -F: '{if ($2 %2 == 0) {sub($4,"/bin/zsh")} print $0}' ./passwd_new
